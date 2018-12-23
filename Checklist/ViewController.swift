6a50307a8a61b3d67dcd0447217d2c0f00365a07
Ilya Fedorchenko <ifedorchenko50@yandex.ru>
2018-12-24 02:02:42 +0300

    Initial Commit

---
+++ Checklist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Checklist
+//
+//  Created by Mac on 24/12/2018.
+//  Copyright © 2018 HardSoft inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
